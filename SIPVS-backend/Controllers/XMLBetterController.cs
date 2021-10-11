08c31268bf65a2214761a025f8381f4282d8959b
williambrach <wibrach@gmail.com>
2021-10-11 12:10:01 +0200

    snad toto uz funguje

---
+++ SIPVS-backend/Controllers/XMLBetterController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SIPVS_backend.Handlers;
+using HttpGetAttribute = Microsoft.AspNetCore.Mvc.HttpGetAttribute;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+
+namespace SIPVS_backend.Controllers
+{
+    [Route("api/xml2")]
+    [ApiController]
+    public class XMLBetterController : ApiController
+    {
+
+        // GET: api/<XMLController>
+        [Route("isvalid")]
+        [HttpGet]
+        public bool isXMLValid()
+        {
+            XMLHandler handler = new XMLHandler();
+            bool isValid = handler.isXMLValid();
+            return true;
+        }
+
+        [Route("savexml")]
+        [HttpGet]
+        public async Task<FileStreamResult> saveXML()
+        {
+            XMLHandler handler = new XMLHandler();
+            var stream = await handler.createXML();
+            //if (String.IsNullOrEmpty(id))
+            //    return Request.CreateResponse(HttpStatusCode.BadRequest);
+
+
+            return new FileStreamResult(stream, "application/octet-stream");
+
+        }
+
+        [Route("visualize")]
+        [HttpGet]
+        public async Task<FileStreamResult> visualizeXML()
+        {
+            XMLHandler handler = new XMLHandler();
+            var stream = await handler.createHTML();
+            return new FileStreamResult(stream, "application/octet-stream");
+
+        }
+
+    }
+}
---
+++ SIPVS-backend/Handlers/XMLHandler.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SIPVS_backend.Handlers
+{
+    public class XMLHandler
+    {
+
+        public IEnumerable<string> test() { 
+            return new string[] { "value1", "value2" };
+        }
+
+        public bool isXMLValid() {
+            bool isValid = false;
+            return isValid;
+        }
+
+        public async Task<FileStream> createXML() { 
+            string fileName = "formular.xml";
+            string localFilePath = @"C:\Users\william.brach\Developer\SIPVS-backend\XML\formular.xml";
+            var stream = File.OpenRead(localFilePath);
+            return stream;
+        
+        }
+
+        public async Task<FileStream> createHTML()
+        {
+            string fileName = "formular.xml";
+            string localFilePath = @"C:\Users\william.brach\Developer\SIPVS-backend\XML\formular.xml";
+            var stream = File.OpenRead(localFilePath);
+            return stream;
+
+
+        }
+
+    }
+}
