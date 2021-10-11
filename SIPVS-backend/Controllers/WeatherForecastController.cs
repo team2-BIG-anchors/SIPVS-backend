32b84888dc2a20b7a3333a012a99e518a5db5d9f
williambrach <wibrach@gmail.com>
2021-10-11 10:35:57 +0200

    advanced setup + basic routing

--- SIPVS-backend/Controllers/WeatherForecastController.cs
+++ SIPVS-backend/Controllers/WeatherForecastController.cs
@@ -24,16 +24,17 @@ namespace SIPVS_backend.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public String Get()
         {
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            //return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            //{
+            //    Date = DateTime.Now.AddDays(index),
+            //    TemperatureC = rng.Next(-20, 55),
+            //    Summary = Summaries[rng.Next(Summaries.Length)]
+            //})
+            //.ToArray();
+            return "KAPPA";
         }
     }
 }
